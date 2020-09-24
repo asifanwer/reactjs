a39d6cc8fff70ba4f3bdc6c0d9bd1a52d20051ae
asifanwer <71032207+asifanwer@users.noreply.github.com>
2020-09-24 12:57:18 +0530

    initial commit

---
+++ JavaScript/WebContent/WEB-INF/Dom.js
@@ -0,0 +1,10 @@
+
+    "use strict";
+
+function mphTokmph(kmph){
+	console.log("MPH value entered : ",mph);
+		return 1.61*mph;
+		
+}
+let speedLimit=mphTokmph(65);
+console.log("speed limit is km/h: ",speedLimit);
---
+++ JavaScript/src/Dom.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+document.getElementById("numericsection").innerHTML=30;
+document.getElementById("textsection").innerHTML="Name:Asif";
+
+var a;
+a=10;
+var b,c;
+b=c=20;
+x=a+30;
+console.log("value of a:" +a);
+console.log("value of b:" +b);
+console.log("value of c:" +c);
+console.log("value of x:" +x);
